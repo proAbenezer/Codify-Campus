56bc8433ea6317f5b25b730deb70ecf25d287422
Rasmus L <ras.lehtonen@gmail.com>
2023-11-25 09:06:19 +0200

    some initial work

---
+++ backend/controllers/users.js
@@ -0,0 +1,27 @@
+const bcrypt = require("bcrypt");
+const usersRouter = require("express").Router();
+const User = require("../models/user");
+
+usersRouter.post("/", async (request, response) => {
+  const { username, name, password } = request.body;
+  const passwordHash = await bcrypt.hash(password, 10);
+  const user = new User({
+    username,
+    name,
+    passwordHash,
+  });
+
+  const savedUser = await user.save();
+
+  response.status(201).json(savedUser);
+});
+
+usersRouter.get("/", async (_, response) => {
+  const users = await User.find({}).populate("notes", {
+    content: 1,
+    important: 1,
+  });
+  response.json(users);
+});
+
+module.exports = usersRouter;
---
+++ backend/utils/middleware.js
@@ -0,0 +1,44 @@
+const requestLogger = (request, _, next) => {
+  console.log("Method: ", request.method);
+  console.log("Path:   ", request.path);
+  console.log("Body:   ", request.body);
+  console.log("---");
+  next();
+};
+
+const unknownEndpoint = (_, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
+const errorHandler = (error, _, response, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  } else if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message });
+  } else if (error.name === "JsonWebTokenError") {
+    return response.status(400).json({ error: error.message });
+  }
+
+  next(error);
+};
+
+const authMiddleware = (request, response, next) => {
+  const token = request.get("authorization");
+  token =
+    token && token.startsWith("Bearer ") ? token.replace("Bearer ", "") : null;
+  const decodedToken = jwt.verify(authorization, process.env.JWT_SECRET);
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: "token invalid" });
+  }
+  request.user = decodedToken;
+  next();
+};
+
+module.exports = {
+  requestLogger,
+  unknownEndpoint,
+  errorHandler,
+  authMiddleware,
+};
